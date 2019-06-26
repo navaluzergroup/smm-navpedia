d405956f7df89642d991a322c6bed652727cd8dd
navaluzergroup <navaluzergroup@gmail.com>
2019-06-26 10:10:05 +0800

    Edit File Tes

--- tes.php
+++ tes.php
@@ -1,6 +1,6 @@
 <?php
 
-/* $config['cookie'] = 'ds_user=akmal.mf;rur=FRC;csrftoken=O9ksG5pDewLFmv9B0meqygMT62rI7YfI;mid=WV-BfwABAAGuF8S1VIzu9N8aQ3GS;ds_user_id=3203344135;sessionid=IGSC43c04d91baf06907a32131b04867af636eed66b887c0b2e5401ec8583d008752%3AG0G1oXaaPpBPPpkUnspw4w1cChK9tFgG%3A%7B%22_auth_user_id%22%3A3203344135%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_token_ver%22%3A2%2C%22_token%22%3A%223203344135%3A2ZSfMf5KZFSn60j1kLMAnzTZ4B8ux7TN%3A837ce0002e4b0e7433faabf4b34f9952c01d92b825c9d928b2d4bef6e1996172%22%2C%22_platform%22%3A1%2C%22last_refreshed%22%3A1499431296.2385597229%7D;';
+$config['cookie'] = 'ds_user=akmal.mf;rur=FRC;csrftoken=O9ksG5pDewLFmv9B0meqygMT62rI7YfI;mid=WV-BfwABAAGuF8S1VIzu9N8aQ3GS;ds_user_id=3203344135;sessionid=IGSC43c04d91baf06907a32131b04867af636eed66b887c0b2e5401ec8583d008752%3AG0G1oXaaPpBPPpkUnspw4w1cChK9tFgG%3A%7B%22_auth_user_id%22%3A3203344135%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_token_ver%22%3A2%2C%22_token%22%3A%223203344135%3A2ZSfMf5KZFSn60j1kLMAnzTZ4B8ux7TN%3A837ce0002e4b0e7433faabf4b34f9952c01d92b825c9d928b2d4bef6e1996172%22%2C%22_platform%22%3A1%2C%22last_refreshed%22%3A1499431296.2385597229%7D;';
 $config['useragent'] = 'Instagram 10.15.0 Android (24/7.0; 272dpi; 720x1193; LGE/MetroPCS; LGMP260; lv517; lv517; en_US)';
 
 
@@ -61,24 +61,24 @@ return $x;
 
 
 $target = "https://www.instagram.com/p/Bw1JzXVlNPK/?utm_source=ig_web_copy_link";
- */
+ 
 
-    /* $cek = chek_post($target); */
-    /* echo "<pre>";
+     $cek = chek_post($target); */
+     echo "<pre>";
     echo print_r($cek);
     echo "</pre>"; */
     
-    /* echo "<br/>";
+     echo "<br/>";
    $usernya = $cek->media_id;
     $media_info = proccess(1, $config['useragent'], 'media/'.$usernya.'/info/', $config['cookie']);
     echo print_r($media_info); */
-/* 
+
     $result = fetchData('https://www.instagram.com/saxrzky/');
     echo "<pre>";
     json_decode($result);
     echo "</pre>"; */
 
-/* echo realpath(dirname(__DIR__)); */
+echo realpath(dirname(__DIR__)); 
 
 
 if (filter_var('rezkyforce@g@mail.com', FILTER_VALIDATE_EMAIL)) { 
